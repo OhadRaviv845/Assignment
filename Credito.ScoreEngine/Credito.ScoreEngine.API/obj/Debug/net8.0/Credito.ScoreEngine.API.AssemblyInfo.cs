e17f6c4ea2f1e1192ba9356cf6fbed8ee6c8a628
Ohad Raviv <84383308+OhadRaviv845@users.noreply.github.com>
2025-02-04 09:30:03 +0000

    Final

--- Credito.ScoreEngine/Credito.ScoreEngine.API/obj/Debug/net8.0/Credito.ScoreEngine.API.AssemblyInfo.cs
+++ Credito.ScoreEngine/Credito.ScoreEngine.API/obj/Debug/net8.0/Credito.ScoreEngine.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Credito.ScoreEngine.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5baa22e92c7c911aa3748a6d3ad742273a3c82e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b4b500f71289f15aaa0b90a257a25d03f0b4741")]
 [assembly: System.Reflection.AssemblyProductAttribute("Credito.ScoreEngine.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Credito.ScoreEngine.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
