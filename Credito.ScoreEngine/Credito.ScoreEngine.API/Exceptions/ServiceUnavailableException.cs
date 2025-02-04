b136a9963b78230b492c9204deaf8337976ad978
Ohad Raviv <84383308+OhadRaviv845@users.noreply.github.com>
2025-02-04 08:28:02 +0000

    Final commit

--- Credito.ScoreEngine/Credito.ScoreEngine.API/Exceptions/ServiceUnavailableException.cs
+++ Credito.ScoreEngine/Credito.ScoreEngine.API/Exceptions/ServiceUnavailableException.cs
@@ -2,7 +2,7 @@ namespace Credito.ScoreEngine.API.Exceptions;
 
 public class ServiceUnavailableException : Exception
 {
-    public ServiceUnavailableException(string message, Exception inner = null) 
+    public ServiceUnavailableException(string message, Exception? inner = null) 
         : base(message, inner)
     {
     }
